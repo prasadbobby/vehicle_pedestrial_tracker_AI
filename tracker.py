2942f45c1befc30d264e56fddf66cd76eee18ee8
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-06-08 12:22:12 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,4 +4,6 @@ img_file = 'car1.jpeg'
 classifier = "car_classifier.xml"                     # pre-trained car-classifier algorithm
 
 img = cv2.imread(img_file)
+cv2.imread('Vehicle_pedestrial_tracker', img)
+cv2.waitKey()
 print('Hello world')
