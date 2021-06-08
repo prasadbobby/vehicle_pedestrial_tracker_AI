cdb0fbc7e47a0adc7745b3147d33e8cb925ce1cd
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-06-08 12:19:27 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,5 +1,7 @@
-import cv2                                      # importing cv2 module to code
+import cv2                                            # importing cv2 module to code
 
-img = "car1.jpeg"                                
+img_file = 'car1.jpeg'                               
 classifier = "car_classifier.xml"                     # pre-trained car-classifier algorithm
+
+img = cv2.imread(img_file)
 print('Hello world')
